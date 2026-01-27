21c74798efb11e4dad23abc2365aef1a7041c165
YH7916 <3188127343@qq.com>
2026-01-27 20:52:46 +0800

    更改worker子域名

--- utils/constants.ts
+++ utils/constants.ts
@@ -9,7 +9,7 @@ export const WORKER_URL = 'https://api.yesterhaze.codes';
 // 1. 注释掉那个 http 的 IP 地址
 // 2. 启用你的 Cloudflare Worker 地址 (就是你刚才部署的那个)
 // 注意：Cloudflare Worker 不需要加 /api/analyze，因为 Worker 代码里已经写死转发了
-export const CLOUD_AI_URL = 'https://cutego-proxy.3240106155.workers.dev'; 
+export const CLOUD_AI_URL = 'https://proxy.yesterhaze.codes'; 
 
 // export const CLOUD_AI_URL = 'http://39.104.59.160:8080/api/analyze';
 // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
